cd7ecf66d041a155224012c5120cb2d5629d4e0d
Luc Bocahut <lucbocahut@gmail.com>
2024-03-14 22:22:19 +0100

    cleanup

--- server/connectors/mongo.ts
+++ server/connectors/mongo.ts
@@ -1,35 +1,35 @@
-import { MongoClient, ObjectId } from 'mongodb'
+// import { MongoClient, ObjectId } from 'mongodb'
 
-const uri = process.env.MONGO_URI || ''
-const client = new MongoClient(uri)
+// const uri = process.env.MONGO_URI || ''
+// const client = new MongoClient(uri)
 
-export const DB = async () => {
-  await client.connect()
-  return client.db('map')
-}
+// export const DB = async () => {
+//   await client.connect()
+//   return client.db('map')
+// }
 
-export const ProjectsCollection = async () => {
-  await client.connect()
-  return client.db('map').collection('Projects')
-}
+// export const ProjectsCollection = async () => {
+//   await client.connect()
+//   return client.db('map').collection('Projects')
+// }
 
-export const TokensCollection = async () => {
-  await client.connect()
-  return client.db('map').collection('Tokens')
-}
+// export const TokensCollection = async () => {
+//   await client.connect()
+//   return client.db('map').collection('Tokens')
+// }
 
-export const GetCollection = async (collectionName: string) => {
-  await client.connect()
-  return client.db('map').collection(collectionName)
-}
+// export const GetCollection = async (collectionName: string) => {
+//   await client.connect()
+//   return client.db('map').collection(collectionName)
+// }
 
-export { ObjectId }
+// export { ObjectId }
 
-export default {
-  DB,
-  MongoClient,
-  ObjectId,
-  GetCollection,
-  ProjectsCollection,
-  TokensCollection
-}
+// export default {
+//   DB,
+//   MongoClient,
+//   ObjectId,
+//   GetCollection,
+//   ProjectsCollection,
+//   TokensCollection
+// }
